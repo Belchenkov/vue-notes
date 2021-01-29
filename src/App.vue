4fbe1fa1fdde014b48b8e9cc71d4457635bd40bf
Aleksey Belchenkov <u608110@gmail.com>
2021-01-29 22:06:04 +0300

    Move

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
 <template>
   <div class="wrapper">
     <div class="wrapper-content">
-      <section>
-        <div class="container">
-          <img alt="Vue logo" src="./assets/logo.png">
+      <div class="container">
+        <h1>{{ title }}</h1>
+        <div class="message" v-if="message">
+          <p>{{ message }}</p>
         </div>
-      </section>
+        <div class="new-note">
+          <input v-model="note.title" type="text" />
+          <textarea v-model="note.desc"></textarea>
+          <button @click="addNote">New Note</button>
+        </div>
+        <div class="notes">
+          <div class="note" v-for="(note, idx) in notes" :key="idx">
+            <div class="note-header">
+              <p>{{ note.title }}</p>
+            </div>
+            <div class="note-body">
+              <p>{{ note.desc }}</p>
+              <span>{{ note.date }}</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -15,8 +32,57 @@
 export default {
   name: 'App',
   components: {
+  },
+  data() {
+    return {
+      title: 'Notes Vue',
+      note: {
+        title: '',
+        desc: ''
+      },
+      message: null,
+      notes: [
+        {
+          title: 'First Note',
+          desc: 'Desc for first note',
+          date: new Date(Date.now()).toLocaleString()
+        },
+        {
+          title: 'Second Note',
+          desc: 'Desc for Second note',
+          date: new Date(Date.now()).toLocaleString()
+        },
+        {
+          title: 'Third Note',
+          desc: 'Desc for Third note',
+          date: new Date(Date.now()).toLocaleString()
+        }
+      ]
+    }
+  },
+  methods: {
+    addNote() {
+      if (this.note.title === '') {
+        this.message = 'Empty title';
+        return false;
+      }
+
+      this.notes.push({
+        ...this.note,
+        date: new Date(Date.now()).toLocaleString()
+      });
+
+      this.note = {
+        title: '',
+        desc: ''
+      };
+
+      this.message = null;
+
+    }
   }
 }
+
 </script>
 
 <style>
